8c0cad464206106100b39258481cf8c2227f2596
Gosula Venkatanagi Reddy <gvnagireddy20@gmail.com>
2026-07-30 02:54:17 +0530

    completed day 3 arrays and strings

---
+++ ArraysAndStrings/Program.cs
@@ -0,0 +1,14 @@
+﻿string [ ] names = new string[5];
+for (int i =0; i<names.Length; i++)
+{
+    Console.WriteLine($"Enter name {i+1}: ");
+    names[i] = Console.ReadLine();
+}
+Console.WriteLine();
+Console.WriteLine("Nmaes In Reverse Order: ");
+
+for (int i = names.Length - 1; i >= 0; i --)
+
+{
+    Console.WriteLine(names[i].ToUpper());
+}
